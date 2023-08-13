d5a357c0c9713c3a077a19fc001b17bdff737d63
Построение изображения предмета в тонкой линзе <erhova.anastacia@yandex.ru>
2023-08-13 18:31:47 +0500

    initial commit

---
+++ src/main/java/com/example/group_pr/GroupPrApplication.java
@@ -0,0 +1,13 @@
+package com.example.group_pr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupPrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupPrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/group_pr/GroupPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.group_pr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupPrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
